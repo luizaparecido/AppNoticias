da6c3f393cf55caf47bd7e7eb814ec83164eee95
j.luiz <luizjaparecido@gmail.com>
2023-07-07 20:08:59 -0300

    Initial Commit

---
+++ AppNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppNews
+//
+//  Created by Luiz Aparecido Joana on 07/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
